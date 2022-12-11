7be6ceb33b1f38075726c85e23834385937cf9eb
Pradhyum R <drpradhyum2016@outlook.com>
2022-12-11 22:17:45 +0000

    James's changes

--- build-script.py
+++ build-script.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from shutil import copy
 import webbrowser as wb
 import os
+import sys
 
 env = Environment(
         loader = FileSystemLoader('.'),
@@ -11,7 +12,8 @@ env = Environment(
 )
 
 files = 'about-us.html contact-us.html donate.html index.html updates.html new.html'.split()
-static = 'pradhyum.jpg rand.jfif templogo.png weblogo.png favicon.png athmik.png athmik-a.jpg james.jpg liann.png pradhyum_actual.jpg athmik-actual.JPG favicon.png sendmail.php features.css style.css'.split()
+static = 'pradhyum.jpg rand.jfif templogo.png weblogo.png favicon.png athmik.png athmik-a.jpg james.jpg liann.jpg pradhyum_actual.jpg \
+athmik-actual.JPG favicon.png sendmail.php features.css s.css'.split()
 output_dir = Path('build')
 if not output_dir.exists():
     output_dir.mkdir()
@@ -24,5 +26,5 @@ for filename in static:
     copy(filename, output_dir / filename)
 
 print('\033[0;32mSuccess!\033[0m')
-#if input('\033[0;33mOpen build? \033[34m')[0].lower() == 'y':
-#    wb.open('file://' + os.path.realpath('build/index.html'))
+if sys.argv[1:] == ['-o']:
+    wb.open('file://' + os.path.realpath('build/index.html'))
